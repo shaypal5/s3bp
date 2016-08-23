bcc1ca0471a9a1eba92d6cccf2f21e2bfd8c2f4a
Shay Palachy <shaypal5@gmail.com>
2016-08-24 02:11:31 +0300

    now only importing public functions

--- s3bp/__init__.py
+++ s3bp/__init__.py
@@ -1,4 +1,15 @@
 """This package enables saving and loading of pandas DataFrame objects to disk
 while also backing to S3 storage. """
 
-from .s3bp import *  # pylint: disable=W0401
+from .s3bp import (
+    set_max_workers,
+    set_default_bucket,
+    unset_default_bucket,
+    set_default_base_directory,
+    map_base_directory_to_bucket,
+    remove_base_directory_mapping,
+    upload_file,
+    download_file,
+    save_dataframe,
+    load_dataframe
+)
