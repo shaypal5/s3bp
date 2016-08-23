36c87e3320e94e723f9d9973b08dd2d4ff475441
Shay Palachy <shaypal5@gmail.com>
2016-08-24 02:23:48 +0300

    version update

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
     name='s3bp',
     packages=['s3bp'],
-    version='0.1',
+    version='0.1.1',
     description='Read and write pandas DataFrames from/to S3.',
     author='Shay Palachy',
     author_email='shaypal5@gmail.com',
     url='https://github.com/shaypal5/s3bp',
-    download_url='https://github.com/shaypal5/s3bp/tarball/0.1',
+    download_url='https://github.com/shaypal5/s3bp/tarball/0.1.1',
     keywords=['pandas', 'dataframe', 's3'],
     classifiers=[],
 )
