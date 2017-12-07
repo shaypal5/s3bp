e2a164ebc24a46f0c8de94d810663d946dabe6f8
Shay Palachy <shaypal5@gmail.com>
2017-12-07 20:02:25 +0200

    added missing save_object() and load_object() to __init__.py

--- s3bp/__init__.py
+++ s3bp/__init__.py
@@ -10,6 +10,8 @@ from .core import (
     remove_base_directory_mapping,
     upload_file,
     download_file,
+    save_object,
+    load_object,
     save_dataframe,
     load_dataframe
 )
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=[
         'botocore',
         'boto3',
-        'dateutil',
+        'python-dateutil',
         'pyyaml',
         'pandas',
         'feather-format'
